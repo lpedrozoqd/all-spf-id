752062d5ce3822a4b23c2adb9a519477e45ab3bc
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-07 22:05:03 -0500

    Modificación de repositorio

---
+++ src/main/java/edu/clarape/al/allspfid/AllSpfIdApplication.java
@@ -0,0 +1,13 @@
+package edu.clarape.al.allspfid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSpfIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSpfIdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/clarape/al/allspfid/AllSpfIdApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.clarape.al.allspfid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSpfIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
