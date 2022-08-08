1e0aaefb31b2d437cb21ba780a2b361f200fb187
Leonel Pedrozo Quiroz <leonel.pedrozo.quiroz.d@gmail.com>
2022-08-07 22:23:45 -0500

    Ejemplo de 'inversión de control'

--- src/main/java/edu/clarape/al/allspfid/AllSpfIdApplication.java
+++ src/main/java/edu/clarape/al/allspfid/AllSpfIdApplication.java
@@ -2,12 +2,21 @@ package edu.clarape.al.allspfid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import edu.clarape.al.allspfid.controller.MyController;
 
 @SpringBootApplication
 public class AllSpfIdApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AllSpfIdApplication.class, args);
+		//Asignación inicial:
+		//SpringApplication.run(AllSpfIdApplication.class, args);
+		
+		ApplicationContext ctx = SpringApplication.run(AllSpfIdApplication.class, args);
+		MyController myController =  (MyController)ctx.getBean("myController");
+		String myControler = myController.sayHello();
+		System.out.println(myControler);
 	}
 
 }
---
+++ src/main/java/edu/clarape/al/allspfid/controller/MyController.java
@@ -0,0 +1,11 @@
+package edu.clarape.al.allspfid.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("### Hello world!");
+        return ">>> Hello folks!";
+    }
+}
